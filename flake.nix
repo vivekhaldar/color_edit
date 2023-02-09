38a903603bda1ff443ce4978fee296bf4a5454c9
Vivek Haldar <haldar@Viveks-Air.lan>
2023-02-08 22:42:32 -0800

    color_edit: poetry config that can run it + nix flake (shell only).

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Example Python development environment for Zero to Nix";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/ARM Linux
+        "aarch64-linux" # 64-bit AMD Linux
+        "x86_64-darwin" # 64-bit Intel/ARM macOS
+        "aarch64-darwin" # 64-bit Apple Silicon
+      ];
+
+      # Helper to provide system-specific attributes
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            # Use Python 3.11
+            python = pkgs.python311;
+          in
+          pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = [
+              # Python plus helper tools
+              (python.withPackages (ps: with ps; [
+                virtualenv # Virtualenv
+                pip # The pip installer
+                poetry # will do poetry2nix later
+              ]))
+            ];
+          };
+      });
+
+      # Build the Python app binary.
+      # packages = forAllSystems ({ pkgs }: {
+      #   default =
+      #     let
+      #       python = pkgs.python311;
+      #     in
+      #     python.pkgs.buildPythonApplication {
+      #       name = "copy_pics";
+      #       buildInputs = with python.pkgs; [ pip ];
+      #       src = ./.;
+      #     };
+      # });
+    };
+}

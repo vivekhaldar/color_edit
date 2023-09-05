7cea7f640e6311384c00c9cfd3c817e7fb22b196
Vivek Haldar <vh@vivekhaldar.com>
2023-09-04 17:46:29 -0700

    Cleanups in find_speaking_intervals.

--- color_edit.py
+++ color_edit.py
@@ -116,6 +116,9 @@ def color_edit(vid_file_clip):
     return color_edited_video, intervals_to_keep
 
 
+# Note: following refactor was suggested by ChatGPT (GPT-4)
+# https://chat.openai.com/share/b5a78779-2d97-4526-82d2-69afa05ab43b
+
 # Iterate over audio to find the non-silent parts. Outputs a list of
 # (speaking_start, speaking_end) intervals.
 # Args:
@@ -123,35 +126,31 @@ def color_edit(vid_file_clip):
 #  volume_threshold: volume below this threshold is considered to be silence
 #  ease_in: (in seconds) add this much silence around speaking intervals
 def find_speaking_intervals(audio_clip, window_size=0.1, volume_threshold=0.01, ease_in=0.1, audio_fps=44100):
-    # First, iterate over audio to find all silent windows.
-    num_windows = math.floor(audio_clip.end/window_size)
-    window_is_silent = []
-    for i in range(num_windows):
-        s = audio_clip.subclip(i * window_size, (i + 1) * window_size).set_fps(audio_fps)
-        v = s.max_volume()
-        window_is_silent.append(v < volume_threshold)
+    num_windows = math.floor(audio_clip.end / window_size)
 
-    # Find speaking intervals.
-    speaking_start = 0
-    speaking_end = 0
+    # Find silent windows using list comprehension
+    window_is_silent = [
+        audio_clip.subclip(i * window_size, (i + 1) * window_size).set_fps(audio_fps).max_volume() < volume_threshold
+        for i in range(num_windows)
+    ]
+    
     speaking_intervals = []
-    for i in range(1, len(window_is_silent)):
-        e1 = window_is_silent[i - 1]
-        e2 = window_is_silent[i]
+    speaking_start = 0
+
+    # Iterate over adjacent elements using zip
+    for i, (e1, e2) in enumerate(zip(window_is_silent[:-1], window_is_silent[1:])):
         # silence -> speaking
         if e1 and not e2:
-            speaking_start = i * window_size
+            speaking_start = (i + 1) * window_size
         # speaking -> silence, now have a speaking interval
         if not e1 and e2:
-            speaking_end = i * window_size
-            new_speaking_interval = [max(0, speaking_start - ease_in), speaking_end + ease_in]
+            speaking_end = (i + 1) * window_size
+            new_interval = [max(0, speaking_start - ease_in), speaking_end + ease_in]
             # With tiny windows, this can sometimes overlap the previous window, so merge.
-            need_to_merge = len(speaking_intervals) > 0 and speaking_intervals[-1][1] > new_speaking_interval[0]
-            if need_to_merge:
-                merged_interval = [speaking_intervals[-1][0], new_speaking_interval[1]]
-                speaking_intervals[-1] = merged_interval
+            if speaking_intervals and speaking_intervals[-1][1] > new_interval[0]:
+                speaking_intervals[-1][1] = new_interval[1]
             else:
-                speaking_intervals.append(new_speaking_interval)
+                speaking_intervals.append(new_interval)
 
     return speaking_intervals
 

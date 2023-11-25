a0cc8b62e7cee09422460fd0a74979872d4ac2e4
Vivek Haldar <vh@vivekhaldar.com>
2023-11-25 13:18:55 -0800

    Fix bug in audio normalization.

--- edit_normalize.sh
+++ edit_normalize.sh
@@ -13,13 +13,15 @@ output_file="${input_file%.*}.edited.${input_file##*.}"
 # Using ffmpeg command line, find the max volume in output file and store it in a variable.
 max_volume=$(ffmpeg -i $output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "max_volume" | awk '{print $5}')
 
-echo "Max volume: $max_volume"
-
+echo "=========> Max volume: $max_volume"
 
 # Construct filename of normalized output file.
 normalized_output_file="${input_file%.*}.normalized.${input_file##*.}"
 
 # Using ffmpeg command line, normalize the audio in the output file.
-ffmpeg -y -i $output_file -af "volume=$max_volume" -c:v copy -c:a aac -b:a 192k  $normalized_output_file
+ffmpeg -i $output_file -af loudnorm $normalized_output_file
 
+max_volume_after_normalization=$(ffmpeg -i $normalized_output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "max_volume" | awk '{print $5}')
+
+echo "==========> Max volume AFTER normalization: $max_volume_after_normalization"
 

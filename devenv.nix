df9f839c6d94008e090bfc8a41d56beb1afaf6b5
Vivek Haldar <vh@vivekhaldar.com>
2023-12-18 21:32:41 -0800

    Trying devenv + nix; numpy still broken!

---
+++ devenv.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+
+{
+        languages.python.enable = true;
+        languages.python.version = "3.11.6";
+        languages.python.venv.enable = true;
+        languages.python.venv.requirements = "moviepy \n pytest \n openai-whisper ";
+}

1b2ed4584b1b4f91aef3807614ba575adad86e27
Vivek Haldar <vh@vivekhaldar.com>
2023-12-19 20:17:44 -0800

    devenv: editing works after adding libz.

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }:
 
 {
-        languages.python.enable = true;
-        languages.python.version = "3.11.6";
-        languages.python.venv.enable = true;
-        languages.python.venv.requirements = "moviepy \n pytest \n openai-whisper ";
+  packages = [ 
+    pkgs.zlib
+    pkgs.libz
+  ];
+  languages.python.enable = true;
+  languages.python.version = "3.11.6";
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = "moviepy \n pytest \n openai-whisper ";
 }

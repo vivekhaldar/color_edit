528c34dc57f72b71f10625943fad1e3d47bb7256
Vivek Haldar <vh@vivekhaldar.com>
2024-01-02 21:59:04 -0800

    Use speechnorm, not loudnorm.

--- edit_normalize.sh
+++ edit_normalize.sh
@@ -10,16 +10,18 @@ output_file="${input_file%.*}.edited.${input_file##*.}"
 
 # Using ffmpeg command line, find the max volume in output file and store it in a variable.
 max_volume=$(ffmpeg -i $output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "max_volume" | awk '{print $5}')
-
 echo "=========> Max volume: $max_volume"
+mean_volume=$(ffmpeg -i $output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "mean_volume" | awk '{print $5}')
+echo "=========> Mean volume: $mean_volume"
 
 # Construct filename of normalized output file.
 normalized_output_file="${input_file%.*}.normalized.${input_file##*.}"
 
 # Using ffmpeg command line, normalize the audio in the output file.
-ffmpeg -i $output_file -af loudnorm $normalized_output_file
+ffmpeg -i $output_file -af "speechnorm=e=12.5:r=0.0001:l=1" $normalized_output_file
 
-max_volume_after_normalization=$(ffmpeg -i $normalized_output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "max_volume" | awk '{print $5}')
-
-echo "==========> Max volume AFTER normalization: $max_volume_after_normalization"
-
+echo "=======> Volumes after normalization"
+max_volume=$(ffmpeg -i $normalized_output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "max_volume" | awk '{print $5}')
+echo "=========> Max volume: $max_volume"
+mean_volume=$(ffmpeg -i $normalized_output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "mean_volume" | awk '{print $5}')
+echo "=========> Mean volume: $mean_volume"

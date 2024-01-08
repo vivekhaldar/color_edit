3336ab153b620b75f8f5b491f759d8d829502cf9
Vivek Haldar <vh@vivekhaldar.com>
2024-01-07 22:10:20 -0800

    Run through formatter.

--- color_edit.py
+++ color_edit.py
@@ -14,50 +14,56 @@ import numpy as np
 
 # Given a timestamp in seconds, convert to a string in the format HH:MM:SS:FF
 def seconds_to_ts(seconds, fps):
-  hours = math.floor(seconds / 3600)
-  minutes = math.floor((seconds - (hours * 3600)) / 60)
-  secs = math.floor(seconds - (hours * 3600) - (minutes * 60))
-  frames = math.floor((seconds - math.floor(seconds)) * fps)
-  return '{:02d}:{:02d}:{:02d}:{:02d}'.format(hours, minutes, secs, frames)
+    hours = math.floor(seconds / 3600)
+    minutes = math.floor((seconds - (hours * 3600)) / 60)
+    secs = math.floor(seconds - (hours * 3600) - (minutes * 60))
+    frames = math.floor((seconds - math.floor(seconds)) * fps)
+    return "{:02d}:{:02d}:{:02d}:{:02d}".format(hours, minutes, secs, frames)
 
 
 # Export an EDL file given a list of (start, end) time intervals.
 def export_edl(intervals, clip_filename, edl_filename, fps):
-  print('Exporting EDL file: {}'.format(edl_filename))
-  with open(edl_filename, 'w') as f:
-    f.write('TITLE: Timeline 1\n')
-    f.write('FCM: NON-DROP FRAME\n')
-    timeline_seconds = 60 * 60  # Davinci timeline starts at 1 hr.
-    for i, interval in enumerate(intervals):
-      start = interval[0]
-      end = interval[1]
-      duration = end - start
-      timeline_start = timeline_seconds
-      timeline_end = timeline_seconds + duration
-      timeline_seconds = timeline_end
-      f.write('{:03d}  AX       V     C        {} {}  {}  {}\n'.format(
-        i + 1,
-        seconds_to_ts(start, fps),
-        seconds_to_ts(end, fps),
-        seconds_to_ts(timeline_start, fps),
-        seconds_to_ts(timeline_end, fps)))
-      f.write('* FROM CLIP NAME: {}\n'.format(clip_filename))
+    print("Exporting EDL file: {}".format(edl_filename))
+    with open(edl_filename, "w") as f:
+        f.write("TITLE: Timeline 1\n")
+        f.write("FCM: NON-DROP FRAME\n")
+        timeline_seconds = 60 * 60  # Davinci timeline starts at 1 hr.
+        for i, interval in enumerate(intervals):
+            start = interval[0]
+            end = interval[1]
+            duration = end - start
+            timeline_start = timeline_seconds
+            timeline_end = timeline_seconds + duration
+            timeline_seconds = timeline_end
+            f.write(
+                "{:03d}  AX       V     C        {} {}  {}  {}\n".format(
+                    i + 1,
+                    seconds_to_ts(start, fps),
+                    seconds_to_ts(end, fps),
+                    seconds_to_ts(timeline_start, fps),
+                    seconds_to_ts(timeline_end, fps),
+                )
+            )
+            f.write("* FROM CLIP NAME: {}\n".format(clip_filename))
 
 
 # https://chat.openai.com/share/315085b3-d75a-41eb-85c4-57ecfd14fd94
 # Get average RGB of n random pixels in a frame.
 def sample_average_color(frame, n):
     height, width, _ = frame.shape
-    random_coords = [(random.randint(0, height-1), random.randint(0, width-1)) for _ in range(n)]
-    
+    random_coords = [
+        (random.randint(0, height - 1), random.randint(0, width - 1)) for _ in range(n)
+    ]
+
     sampled_pixels = np.array([frame[y, x] for y, x in random_coords])
     average_color = np.mean(sampled_pixels, axis=0)
-    
+
     return average_color
 
 
 # The following refactor was suggested by ChatGPT (GPT-4)
 # https://chat.openai.com/share/53b0cbe4-c651-4cd8-904f-a938593f7662
+
 
 # Enum to label each frame. 'c': content; 'y': keep prior interval; 'n': drop prior interval.
 class FrameMarker(Enum):
@@ -65,9 +71,10 @@ class FrameMarker(Enum):
     KEEP = 2
     DROP = 3
 
+
 def color_edit_intervals(video):
     frame_markers = [
-        get_frame_marker(sample_average_color(frame, 10)) 
+        get_frame_marker(sample_average_color(frame, 10))
         for frame in video.iter_frames()
     ]
 
@@ -91,7 +98,9 @@ def extract_intervals(frame_markers, fps):
     keep_intervals = []
     start_of_last_green = keep_start = keep_end = 0
 
-    for i, (prev_marker, curr_marker) in enumerate(zip(frame_markers, frame_markers[1:])):
+    for i, (prev_marker, curr_marker) in enumerate(
+        zip(frame_markers, frame_markers[1:])
+    ):
         if prev_marker == FrameMarker.CONTENT and curr_marker == FrameMarker.KEEP:
             start_of_last_green = i
 
@@ -116,12 +125,14 @@ def color_edit(vid_file_clip):
     start = timer()
     intervals_to_keep = color_edit_intervals(vid_file_clip)
     print("Keeping color edit intervals: " + str(intervals_to_keep))
-    keep_clips = [vid_file_clip.subclip(start, end) for [start, end] in intervals_to_keep]
+    keep_clips = [
+        vid_file_clip.subclip(start, end) for [start, end] in intervals_to_keep
+    ]
     color_edited_video = concatenate_videoclips(keep_clips)
     end = timer()
 
-    color_edit_time = timedelta(seconds=end-start)
-    print('Color edit time: ' + str(color_edit_time))
+    color_edit_time = timedelta(seconds=end - start)
+    print("Color edit time: " + str(color_edit_time))
 
     return color_edited_video, intervals_to_keep
 
@@ -129,21 +140,27 @@ def color_edit(vid_file_clip):
 # Note: following refactor was suggested by ChatGPT (GPT-4)
 # https://chat.openai.com/share/b5a78779-2d97-4526-82d2-69afa05ab43b
 
+
 # Iterate over audio to find the non-silent parts. Outputs a list of
 # (speaking_start, speaking_end) intervals.
 # Args:
 #  window_size: (in seconds) hunt for silence in windows of this size
 #  volume_threshold: volume below this threshold is considered to be silence
 #  ease_in: (in seconds) add this much silence around speaking intervals
-def find_speaking_intervals(audio_clip, window_size=0.1, volume_threshold=0.005, ease_in=0.1, audio_fps=44100):
+def find_speaking_intervals(
+    audio_clip, window_size=0.1, volume_threshold=0.005, ease_in=0.1, audio_fps=44100
+):
     num_windows = math.floor(audio_clip.end / window_size)
 
     # Find silent windows using list comprehension
     window_is_silent = [
-        audio_clip.subclip(i * window_size, (i + 1) * window_size).set_fps(audio_fps).max_volume() < volume_threshold
+        audio_clip.subclip(i * window_size, (i + 1) * window_size)
+        .set_fps(audio_fps)
+        .max_volume()
+        < volume_threshold
         for i in range(num_windows)
     ]
-    
+
     speaking_intervals = []
     speaking_start = 0
 
@@ -169,14 +186,18 @@ def find_speaking(input_clip, input_audio_fps, volume_threshold=0.005):
     print("\n\n\n----- Now cutting out dead air... -----")
 
     start = timer()
-    speaking_intervals = find_speaking_intervals(input_clip.audio, volume_threshold=volume_threshold, audio_fps=input_audio_fps)
+    speaking_intervals = find_speaking_intervals(
+        input_clip.audio, volume_threshold=volume_threshold, audio_fps=input_audio_fps
+    )
     print("Keeping speaking intervals: " + str(speaking_intervals))
-    speaking_clips = [input_clip.subclip(start, end) for [start, end] in speaking_intervals]
+    speaking_clips = [
+        input_clip.subclip(start, end) for [start, end] in speaking_intervals
+    ]
     final_video = concatenate_videoclips(speaking_clips)
     end = timer()
 
-    speaking_detection_time = timedelta(seconds=end-start)
-    print('Speaking detection time: ' + str(speaking_detection_time))
+    speaking_detection_time = timedelta(seconds=end - start)
+    print("Speaking detection time: " + str(speaking_detection_time))
 
     return final_video, speaking_intervals
 
@@ -186,9 +207,19 @@ def main():
     parser = argparse.ArgumentParser(description="Process a video file.")
 
     # Add the arguments
-    parser.add_argument('--input', metavar='input', type=str, help='the path to the input file')
-    parser.add_argument('--output', metavar='output', type=str, help='the path to the output file')
-    parser.add_argument('--volume_threshold', metavar='volume_threshold', type=float, help='volume threshold for silence detection', default=0.005)
+    parser.add_argument(
+        "--input", metavar="input", type=str, help="the path to the input file"
+    )
+    parser.add_argument(
+        "--output", metavar="output", type=str, help="the path to the output file"
+    )
+    parser.add_argument(
+        "--volume_threshold",
+        metavar="volume_threshold",
+        type=float,
+        help="volume threshold for silence detection",
+        default=0.005,
+    )
 
     # Parse the arguments
     args = parser.parse_args()
@@ -206,38 +237,40 @@ def main():
     color_edited_video, color_intervals = color_edit(vid)
 
     # Cut out dead air.
-    no_dead_air_video, speaking_intervals = find_speaking(color_edited_video, vid.audio.fps, volume_threshold=volume_threshold)
+    no_dead_air_video, speaking_intervals = find_speaking(
+        color_edited_video, vid.audio.fps, volume_threshold=volume_threshold
+    )
 
     # Write out EDL files with intervals.
     clip_name = os.path.split(file_in)[-1]
     clip_dir = os.path.dirname(file_in)
-    color_edl = os.path.join(clip_dir, clip_name + '.color.edl')
-    speaking_edl = os.path.join(clip_dir, clip_name + '.speaking.edl')
+    color_edl = os.path.join(clip_dir, clip_name + ".color.edl")
+    speaking_edl = os.path.join(clip_dir, clip_name + ".speaking.edl")
     export_edl(color_intervals, clip_name, color_edl, fps=vid.fps)
     # The below will not work, because it should be emitting timestamps relative
     # to the color-edited video, but doesn't.
     # export_edl(speaking_intervals, clip_name, speaking_edl)
 
-
     print("\n\n\n----- Writing out edited video... -----")
     start = timer()
-    no_dead_air_video.write_videofile(file_out,
-        #fps=60,
-        preset='ultrafast',
-        codec='libx264',
-        #codec='h264_videotoolbox',
-        temp_audiofile='temp-audio.m4a',
+    no_dead_air_video.write_videofile(
+        file_out,
+        # fps=60,
+        preset="ultrafast",
+        codec="libx264",
+        # codec='h264_videotoolbox',
+        temp_audiofile="temp-audio.m4a",
         remove_temp=True,
         audio_codec="aac",
-        #threads=6,
-        ffmpeg_params = ['-threads', '8'],
+        # threads=6,
+        ffmpeg_params=["-threads", "8"],
     )
     vid.close()
-    end=timer()
+    end = timer()
 
-    render_time = timedelta(seconds=end-start)
-    print('Render time: ' + str(render_time))
+    render_time = timedelta(seconds=end - start)
+    print("Render time: " + str(render_time))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

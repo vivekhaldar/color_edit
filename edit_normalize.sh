084ec9ac42121163103dbd173475c118bb46ae85
Vivek Haldar <vh@vivekhaldar.com>
2024-01-07 21:26:52 -0800

    Shell script takes volume threshold as an arg.

--- edit_normalize.sh
+++ edit_normalize.sh
@@ -3,10 +3,15 @@
 # First argument is raw video file.
 input_file="$1"
 
+# Check if there is a 2nd argument. If so, it is the volume threshold.
+volume_threshold=0.005
+if [ -n "$2" ]; then
+    volume_threshold="$2"
+fi
 # Construct output file name
 output_file="${input_file%.*}.edited.${input_file##*.}"
 
-./color_edit.py --input $input_file --output $output_file
+./color_edit.py --input $input_file --output $output_file --volume_threshold $volume_threshold
 
 # Using ffmpeg command line, find the max volume in output file and store it in a variable.
 max_volume=$(ffmpeg -i $output_file -af "volumedetect"  -vn -sn -dn -f null /dev/null 2>&1 | grep "max_volume" | awk '{print $5}')
